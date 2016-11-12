2dad0cb7ef1163a6b8a8261f15613ac83cfe15c0
Teon Banek <theongugl@gmail.com>
2016-11-12 18:59:32 +0100

    Finish the program manager interface
    For the time being, this should be good enough of an interface for managing
    shaders. In the future, shader and program objects should be wrapped into a
    type which stores additional information. Depending on how the interface feels
    for use, it might be more comfortable if the exported functions took
    prog-manager implicitly via special binding.

--- shake/render-progs.lisp
+++ shake/render-progs.lisp
@@ -14,12 +14,17 @@
 ;;;; with this program; if not, write to the Free Software Foundation, Inc.,
 ;;;; 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+;;; Shader program management: loading, compiling and reusing.
+
 (in-package #:shake.render-progs)
 
 (defun shader-path (name ext)
+  "Return the data path for shaders of given NAME and file extension EXT."
   (sdata:data-path (concatenate 'string "shaders/" name ext)))
 
 (defstruct prog-manager
+  "Manager for shader programs. Loading and using shaders should only be done
+  through a single instance of this type."
   (vertex-shaders (make-hash-table))
   (fragment-shaders (make-hash-table))
   (geometry-shaders (make-hash-table))
@@ -27,84 +32,102 @@
   (bound-program 0 :type fixnum))
 
 (defun init-prog-manager ()
+  "Create and setup a new PROG-MANAGER."
   (make-prog-manager))
 
 (defun shutdown-prog-manager (prog-manager)
+  "Unload and clean any resources related to shader programs."
   (purge-all-programs prog-manager))
 
 (defmacro with-prog-manager (prog-manager &body body)
+  "Setup a new PROG-MANAGER and bind it to given symbol. Perform clean up
+  after the evaluation of BODY."
   `(bracket (,prog-manager (init-prog-manager) shutdown-prog-manager)
      ,@body))
 
 (defun bind-program (prog-manager shader-prog)
+  "Bind the given SHADER-PROG to GL state."
   (unless (= shader-prog (prog-manager-bound-program prog-manager))
     (gl:use-program shader-prog)
     (setf (prog-manager-bound-program prog-manager) shader-prog)))
 
 (defun unbind-program (prog-manager)
+  "Remove any shader program binding from GL state."
   (unless (zerop (prog-manager-bound-program prog-manager))
     (gl:use-program 0)
     (setf (prog-manager-bound-program prog-manager) 0)))
 
 (defun purge-all-programs (prog-manager)
+  "Unload all shader programs. Do not use programs after this function is
+  called, because they will not be loaded. To avoid errors, this function
+  should only be called on shutdown or through RELOAD-PROGRAMS."
   (unbind-program prog-manager)
   (with-struct (prog-manager- vertex-shaders fragment-shaders geometry-shaders
                               programs) prog-manager
     (dolist (shaders (list vertex-shaders fragment-shaders geometry-shaders))
       (maphash-values #'gl:delete-shader shaders))
+    ;; TODO: It would be a good idea to wrap shader and program objects into a
+    ;; type, which stores loaded state so dangling object references cannot be
+    ;; accidentally used. Additional benefit would be to scan and store
+    ;; locations of all active uniforms on the first load.
     (maphash-values #'gl:delete-program programs)))
 
 (defun find-shader (shaders name)
-  (gethash name shaders nil))
+  (gethash name shaders))
 
 (defun add-shader (shaders name gl-shader)
   (setf (gethash name shaders) gl-shader))
 
 (defun load-shader (shader-type name)
-  )
+  "Create and compile a shader of SHADER-TYPE from file NAME."
+  (let* ((ext (ecase shader-type
+               (:vertex-shader ".vert")
+               (:fragment-shader ".frag")
+               (:geometry-shader ".geom")))
+         (source (sdata:with-data-file (file (shader-path name ext))
+                   (read-stream-content-into-string file))))
+    (sgl::compile-shader source shader-type)))
 
 (defun find-program (prog-manager vs-name fs-name gs-name)
-  (gethash (list vs-name fs-name gs-name) (prog-manager-programs prog-manager)
-           nil))
+  (gethash (list vs-name fs-name gs-name) (prog-manager-programs prog-manager)))
 
 (defun add-program (prog-manager vs-name fs-name gs-name gl-prog)
   (setf (gethash (list vs-name fs-name gs-name)
                  (prog-manager-programs prog-manager))
         gl-prog))
 
-(defun load-program (vs-name fs-name &optional gs-name)
-  )
+(defun load-program (prog-manager vs-name fs-name &optional gs-name)
+  "Load and return the linked program using given shader file names."
+  (let ((vs (get-vertex-shader prog-manager vs-name))
+        (fs (get-fragment-shader prog-manager fs-name))
+        (gs (when gs-name (get-geometry-shader prog-manager gs-name))))
+    (apply #'sgl::link-program (cons vs (cons fs (when gs (list gs)))))))
 
 (defun reload-programs (prog-manager)
+  "Recompile and relink all shader programs."
   (purge-all-programs prog-manager)
   (with-struct (prog-manager- vertex-shaders fragment-shaders geometry-shaders
                               programs) prog-manager
     (loop for shaders in (list vertex-shaders fragment-shaders geometry-shaders)
-       and shader-type in (:vertex-shader :fragment-shader :geometry-shader) do
+       and shader-type in '(:vertex-shader :fragment-shader :geometry-shader) do
          (maphash-keys (lambda (name)
-                         (add-shader shaders name
-                                     (load-shader shader-type name)))
+                         (add-shader shaders name (load-shader shader-type name)))
                        shaders))
     (maphash-keys (lambda (shader-names)
-                    (destructuring-bind (vs-name fs-name gs-name) shader-names
-                      (add-program prog-manager vs-name fs-name gs-name
-                                   (load-program vs-name fs-name gs-name))))
+                    (apply #'add-program prog-manager shader-names
+                           (apply #'load-program prog-manager shader-names)))
                   programs)))
 
 (defun get-shader (prog-manager name shader-type)
-  (destructuring-bind (ext . shaders)
-      (ecase shader-type
-        (:vertex-shader
-         (cons ".vert" (prog-manager-vertex-shaders prog-manager)))
-        (:fragment-shader
-         (cons ".frag" (prog-manager-fragment-shaders prog-manager)))
-        (:geometry-shader
-         (cons ".geom" (prog-manager-geometry-shaders prog-manager))))
+  "Get the shader object specified by file NAME and SHADER-TYPE. The shader is
+  loaded if necessary."
+  (let ((shaders
+         (ecase shader-type
+           (:vertex-shader (prog-manager-vertex-shaders prog-manager))
+           (:fragment-shader (prog-manager-fragment-shaders prog-manager))
+           (:geometry-shader (prog-manager-geometry-shaders prog-manager)))))
     (or (find-shader shaders name)
-        (let* ((source (sdata:with-data-file (file (shader-path name ext))
-                         (read-stream-content-into-string file)))
-               (gl-shader (sgl::compile-shader source shader-type)))
-          (add-shader shaders name gl-shader)))))
+        (add-shader shaders name (load-shader shader-type name)))))
 
 (defun get-vertex-shader (prog-manager name)
   (get-shader prog-manager name :vertex-shader))
@@ -116,10 +139,9 @@
   (get-shader prog-manager name :geometry-shader))
 
 (defun get-program (prog-manager vs-name fs-name &optional gs-name)
+  "Get the shader program specified with given shader file names: VS-NAME is a
+  vertex shader, FS-NAME is a fragment shader and GS-NAME is a geometry
+  shader. Shaders are loaded if necessary."
   (or (find-program prog-manager vs-name fs-name gs-name)
-      (let* ((vs (get-vertex-shader prog-manager vs-name))
-             (fs (get-fragment-shader prog-manager fs-name))
-             (gs (when gs-name (get-geometry-shader prog-manager gs-name)))
-             (gl-prog (apply #'sgl::link-program
-                             (cons vs (cons fs (when gs (list gs)))))))
+      (let ((gl-prog (load-program prog-manager vs-name fs-name gs-name)))
         (add-program prog-manager vs-name fs-name gs-name gl-prog))))
