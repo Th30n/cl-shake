5b0c61c94839d816e48b90633f6bc62c97319ff3
Teon Banek <theongugl@gmail.com>
2018-12-09 11:25:15 +0100

    Use single-float in timer

--- shake/debug.lisp
+++ shake/debug.lisp
@@ -5,15 +5,15 @@
 (defstruct timer
   (name "<null>" :type string :read-only t)
   (count 0 :type fixnum)
-  (total 0d0 :type double-float)
-  (new-max 0d0 :type double-float)
-  (max 0d0 :type double-float)
-  (avg 0d0 :type double-float))
+  (total 0.0 :type single-float)
+  (new-max 0.0 :type single-float)
+  (max 0.0 :type single-float)
+  (avg 0.0 :type single-float))
 
 (defun nupdate-timer (timer dt)
   (declare (optimize (speed 3) (space 3)))
   (declare (type timer timer))
-  (declare (type double-float dt))
+  (declare (type single-float dt))
   (incf (timer-count timer))
   (incf (timer-total timer) dt)
   (zap #'max (timer-new-max timer) dt)
@@ -27,8 +27,8 @@
     (setf (timer-avg timer) (/ total count)
           (timer-max timer) new-max
           (timer-count timer) 0
-          (timer-total timer) 0d0
-          (timer-new-max timer) 0d0))
+          (timer-total timer) 0.0
+          (timer-new-max timer) 0.0))
   timer)
 
 (defun performance-delta (start stop)
@@ -36,26 +36,30 @@
   (declare (optimize (speed 3)))
   (declare (type (unsigned-byte 64) start))
   (declare (type (unsigned-byte 64) stop))
-  (/ (coerce (the (unsigned-byte 64) (- stop start)) 'double-float)
+  (/ (coerce (the (unsigned-byte 64) (- stop start)) 'single-float)
      (coerce (the (unsigned-byte 64) (sdl2:get-performance-frequency))
-             'double-float)))
+             'single-float)))
 
-(defun call-with-timer (timer function &key (reset-every 1d0))
+(defun call-with-timer (timer function &key (reset-every 1.0))
   "Upate the TIMER with the execution time of FUNCTION. If the TIMER TOTAL
   exceeds RESET-EVERY seconds, TIMER is reset."
   (declare (optimize (speed 3)))
   (check-type timer timer)
-  (check-type reset-every double-float)
+  (check-type reset-every single-float)
   (check-type function function)
   (let ((start (sdl2:get-performance-counter)))
     (declare (type (unsigned-byte 64) start))
     (multiple-value-prog1 (funcall function)
-      (let* ((end (sdl2:get-performance-counter))
-             (delta (performance-delta start end)))
+      (let ((end (sdl2:get-performance-counter)))
         (declare (type (unsigned-byte 64) end))
-        (nupdate-timer timer delta)
+        ;; Sometimes END is set to a lowe value than START, in such a case
+        ;; ignore the update. This usually happens if the performance counter
+        ;; overflowed, but it seems rather unlikely to happen as often as it
+        ;; does.
+        (when (> end start)
+          (nupdate-timer timer (performance-delta start end)))
         (when (>= (timer-total timer) reset-every)
           (nreset-timer timer))))))
 
-(defmacro with-timer ((timer &key (reset-every 1d0)) &body body)
+(defmacro with-timer ((timer &key (reset-every 1.0)) &body body)
   `(call-with-timer ,timer (lambda () ,@body) :reset-every ,reset-every))
