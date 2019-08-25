a9134c3109122a1391a85b98b0b1a4ef325aa582
Teon Banek <theongugl@gmail.com>
2019-08-25 15:20:01 +0200

    Update building for linux

--- make-shake-executable.lisp
+++ make-shake-executable.lisp
@@ -1,16 +1,16 @@
 (require 'asdf)
-(asdf:load-system :cffi)
-(let ((dst-dir (uiop:merge-pathnames* (format nil "./shake-~A-~A/"
-                                              (if (uiop:os-windows-p) "win" "linux")
-                                              (string-downcase (uiop:architecture))))))
+(let* ((dst-dir (uiop:merge-pathnames* (format nil "./shake-~A-~A/"
+                                               (if (uiop:os-windows-p) "win" "linux")
+                                               (string-downcase (uiop:architecture)))))
+       (built-exe (format nil "shake/shake-command~A" (if (uiop:os-windows-p) ".exe" "")))
+       (dst-exe (uiop:merge-pathnames* (format nil "shake~A" (if (uiop:os-windows-p) ".exe" ""))
+                                       dst-dir)))
   (format t "Building...~%")
   (uiop:delete-directory-tree dst-dir :if-does-not-exist :ignore :validate t)
   (uiop:ensure-all-directories-exist (list dst-dir))
   (uiop:run-program '("sbcl" "--eval" "(asdf:make :shake/executable)" "--quit"))
-  (uiop:rename-file-overwriting-target
-   "shake/shake-command.exe"
-   (uiop:merge-pathnames* "shake.exe" dst-dir))
-  (format t "Created: ~A~%" (uiop:merge-pathnames* "shake.exe" dst-dir))
+  (uiop:rename-file-overwriting-target built-exe dst-exe)
+  (format t "Created: ~A~%" dst-exe)
   (labels ((copy-file (data-path dst-path)
              (uiop:copy-file data-path (uiop:merge-pathnames* dst-path dst-dir))
              (format t "Copied: ~A~%" (uiop:merge-pathnames* dst-path dst-dir)))
